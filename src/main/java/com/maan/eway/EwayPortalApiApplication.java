9a2fe3fb266b01873ddf7c6aa15881a93f1f1c83
Samilla <89253083+MaanRsa001@users.noreply.github.com>
2022-08-22 11:06:08 +0530

    New Project Created

---
+++ src/main/java/com/maan/eway/EwayPortalApiApplication.java
@@ -0,0 +1,13 @@
+package com.maan.eway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EwayPortalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EwayPortalApiApplication.class, args);
+	}
+
+}

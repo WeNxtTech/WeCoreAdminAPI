e10b246951a63293ed7b2c3d99fbf943a5321b96
Samilla <89253083+MaanRsa001@users.noreply.github.com>
2022-09-23 17:07:23 +0530

    Update CustomerSaveValidationServiceImpl.java

--- src/main/java/com/maan/eway/common/service/impl/CustomerSaveValidationServiceImpl.java
+++ src/main/java/com/maan/eway/common/service/impl/CustomerSaveValidationServiceImpl.java
@@ -1,8 +1,20 @@
 package com.maan.eway.common.service.impl;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.maan.eway.common.req.EserviceCustomerSaveReq;
+import com.maan.eway.common.service.CustomerSaveValidationService;
+import com.maan.eway.error.Error;
+
 @Service
-public class CustomerSaveValidationServiceImpl {
+public class CustomerSaveValidationServiceImpl implements CustomerSaveValidationService {
+
+	@Override
+	public List<Error> validateCustomerDetails(EserviceCustomerSaveReq req) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }

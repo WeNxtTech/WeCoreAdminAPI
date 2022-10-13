859ec2c17438b4ac4859ea92175e249a25d7dd60
Kumar <89253083+MaanRsa001@users.noreply.github.com>
2022-10-13 18:34:56 +0530

    Occupation Master

---
+++ src/main/java/com/maan/eway/master/res/OccupationMasterRes.java
@@ -0,0 +1,56 @@
+package com.maan.eway.master.res;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+
+@Data
+public class OccupationMasterRes implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+	@JsonProperty("OccupationId")
+	private String occupationId;
+
+	@JsonProperty("OccupationName")
+	private String occupationName;
+
+	@JsonProperty("OccupationNameAr")
+	private String occupationNameAr;
+	
+	@JsonProperty("CoreAppCode")
+	private String coreAppCode;
+	
+	@JsonProperty("RegulatoryCode")
+	private String regulatoryCode;
+	
+	@JsonFormat(pattern = "dd/MM/yyyy")
+	@JsonProperty("EffectiveDateStart")
+	private Date effectiveDateStart;	
+	
+	@JsonFormat(pattern = "dd/MM/yyyy")
+	@JsonProperty("EffectiveDateEnd")
+	private Date effectiveDateEnd;
+	
+	@JsonProperty("AmendId")
+	private Integer amendId;
+
+	@JsonFormat(pattern = "dd/MM/yyyy")
+	@JsonProperty("EntryDate")
+	private Date entryDate;
+		
+	@JsonProperty("Status")
+	private String status;
+
+	@JsonProperty("Remarks")
+	private String remarks;
+	
+	@JsonProperty("CreatedBy")
+	private String createdBy;
+
+	
+
+}

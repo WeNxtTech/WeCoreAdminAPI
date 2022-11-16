688ca73500a699cb05feac9c6565f2e43ee7c047
Suganya <89253083+MaanRsa001@users.noreply.github.com>
2022-11-16 10:58:21 +0530

    Update CoverDocumentUploadDetails.java

--- src/main/java/com/maan/eway/bean/CoverDocumentUploadDetails.java
+++ src/main/java/com/maan/eway/bean/CoverDocumentUploadDetails.java
@@ -10,6 +10,11 @@
  */
 
 package com.maan.eway.bean;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.IdClass;

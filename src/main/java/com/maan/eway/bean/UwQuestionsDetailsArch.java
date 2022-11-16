693ba4c6a059bfab0c3beb4a743412c94548ee67
Kumar <89253083+MaanRsa001@users.noreply.github.com>
2022-11-16 17:42:40 +0530

    Uw Questions Details

---
+++ src/main/java/com/maan/eway/bean/UwQuestionsDetailsArch.java
@@ -0,0 +1,79 @@
+package com.maan.eway.bean;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="uw_questions_details_arch")
+@IdClass(UwQuestionsDetailsArchId.class)
+public class UwQuestionsDetailsArch {
+
+	@Id
+	@Column(name="ARCH_ID",nullable=false)
+	private Integer archId;
+	
+	
+	@Id
+	@Column(name="COMPANY_ID",length=20,nullable=false)
+	private String companyId;
+	
+	@Id
+	@Column(name="PRODUCT_ID",nullable=false)
+	private Integer productId;
+	
+	@Id
+	@Column(name="REQUEST_REFERENCE_NO",length=20,nullable=false)
+	private String requestReferenceNo;
+	
+	@Id
+	@Column(name="VEHICLE_ID",nullable=false)
+	private Integer vehicleId;
+	
+
+	@Id
+	@Column(name="UW_QUESTION_ID",nullable=false)
+	private Integer uwQuestionId;
+	
+	@Column(name="UW_QUESTION_DESC",length=100)
+	private String uwQuestionDesc;
+	
+	@Column(name="QUESTION_TYPE",length=100)
+	private String questionType;
+	
+
+	@Column(name="VALUE",length=100)
+	private String value;
+	
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name="ENTRY_DATE")
+    private Date       entryDate ;
+
+    @Column(name="STATUS", length=2)
+    private String     status ;
+
+    @Column(name="REMARKS", length=100)
+    private String     remarks ;
+
+    @Column(name="CREATED_BY", length=100)
+    private String     createdBy;
+
+    @Column(name="UPDATED_BY", length=100)
+    private String     updatedBy;
+    
+	
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name="UPDATED_DATE")
+    private Date    updatedDate ;
+	
+	
+}
---
+++ src/main/java/com/maan/eway/master/res/UwQuestionsDetailsRes.java
@@ -0,0 +1,63 @@
+package com.maan.eway.master.res;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+
+@Data
+public class UwQuestionsDetailsRes implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+	@JsonProperty("InsuranceId")
+    private String    companyId ;
+    
+	@JsonProperty("ProductId")
+    private String    productId ;
+    
+	@JsonProperty("RequestReferenceNo")
+    private String    requestReferenceNo ;   
+	
+	@JsonProperty("VehicleId")
+    private String    vehicleId;   
+	
+	@JsonProperty("UwQuestionId")
+    private String   uwQuestionId ;
+    
+	@JsonProperty("UwQuestionDesc")
+    private String   uwQuestionDesc ;
+    
+	@JsonProperty("QuestionType")
+    private String   questionType ;
+    
+	@JsonProperty("Value")
+    private String   value;
+    
+	@JsonProperty("Remarks")
+	private String remarks;
+
+	@JsonProperty("CreatedBy")
+	private String createdBy;
+
+	@JsonProperty("UpdatedBy")
+	private String updatedBy;
+
+	@JsonFormat(pattern="dd/MM/yyyy")
+	@JsonProperty("UpdatedDate")
+	private Date updatedDate;
+
+	@JsonFormat(pattern="dd/MM/yyyy")
+	@JsonProperty("EntryDate")
+	private Date entryDate;
+
+	
+	@JsonProperty("Status")
+	private String status;
+
+}

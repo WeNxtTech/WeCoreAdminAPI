ddb78fcb7081147bfac71b10dad7d24b479dc956
Suganya <89253083+MaanRsa001@users.noreply.github.com>
2023-01-11 13:49:28 +0530

    Update EmiMasterServiceImpl.java

--- src/main/java/com/maan/eway/master/service/impl/EmiMasterServiceImpl.java
+++ src/main/java/com/maan/eway/master/service/impl/EmiMasterServiceImpl.java
@@ -137,9 +137,11 @@ public class EmiMasterServiceImpl implements EmiMasterService {
 				errorList.add(new Error("06", "InstallmentPeriod", "Please Enter InstallmentPeriod"));
 			} else if (!req.getInstallmentPeriod().matches("[0-9.]+")) {
 				errorList.add(new Error("06", "InstallmentPeriod", "Please Enter Valid Number In InstallmentPeriod"));
-			}else if(!("3".equals(req.getInstallmentPeriod()) || "6".equals(req.getInstallmentPeriod())|| "9".equals(req.getInstallmentPeriod()))) {
-				errorList.add(new Error("06", "InstallmentPeriod", "Please Enter InstallmentPeriod Only 3 Or 6 Or 9"));
-			}else {
+			}
+//			else if(!("3".equals(req.getInstallmentPeriod()) || "6".equals(req.getInstallmentPeriod())|| "9".equals(req.getInstallmentPeriod()))) {
+//				errorList.add(new Error("06", "InstallmentPeriod", "Please Enter InstallmentPeriod Only 3 Or 6 Or 9"));
+//			}
+			else {
 				EmiMaster installmentPeriod =getInstallmentPeriod(req.getInstallmentPeriod(),req.getCompanyId(),req.getProductId(),req.getPolicyType());
 				if(StringUtils.isBlank(req.getInstallmentPeriod()) &&  installmentPeriod!=null ) {
 					errorList.add(new Error("08", "InstallmentPeriod", "This InstallmentPeriod  Already Exist"));
@@ -163,8 +165,13 @@ public class EmiMasterServiceImpl implements EmiMasterService {
 				errorList.add(new Error("08", "PremiumStart", "PremiumStart must be greater than PremiumEnd "));
 			}else if (StringUtils.isBlank(req.getInterestPercent())) {
 				errorList.add(new Error("09", "InterestPercent", "Please Enter InterestPercent"));
-			}else if (StringUtils.isBlank(req.getAdvancePercent())) {
+			}else if (Integer.valueOf(req.getInterestPercent())>100) {
+				errorList.add(new Error("09", "InterestPercent", "Please Enter InterestPercent Less that or equal to 100"));
+			}
+			else if (StringUtils.isBlank(req.getAdvancePercent())) {
 				errorList.add(new Error("10", "AdvancePercent", "Please Enter AdvancePercent"));
+			}else if (Integer.valueOf(req.getAdvancePercent())>100) {
+				errorList.add(new Error("10", "AdvancePercent", "Please Enter AdvancePercent Less that or equal to 100"));
 			}
 
 		} catch (Exception e) {

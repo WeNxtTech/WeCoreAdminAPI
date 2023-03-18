a7dd3e845d23a79ee12d46e24732652812e2b952
Suganya <89253083+MaanRsa001@users.noreply.github.com>
2023-03-18 10:36:54 +0530

    coverDocument Save and Get Res

--- src/main/java/com/maan/eway/master/res/CoverDocumentMasterGetRes.java
+++ src/main/java/com/maan/eway/master/res/CoverDocumentMasterGetRes.java
@@ -1,6 +1,7 @@
 package com.maan.eway.master.res;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.Date;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -87,5 +88,35 @@ public class CoverDocumentMasterGetRes implements Serializable {
 
 	@JsonProperty("DocumentName")
     private String   documentName      ;
+	
+    @JsonProperty("EndorsementDate") //EndorsementDate
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private Date       endorsementDate ;
+    @JsonProperty("EndorsementRemarks") // EndorsementRemarks
+    private String     endorsementRemarks ;    
+    @JsonProperty("EndorsementEffectiveDate") // EndorsementEffectiveDate
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private Date       endorsementEffdate ;
+    @JsonProperty("OrginalPolicyNo") // OrginalPolicyNo
+    private String     originalPolicyNo ;
+    @JsonProperty("EndtPrevPolicyNo") // EndtPrevPolicyNo
+    private String     endtPrevPolicyNo ;
+    @JsonProperty("EndtPrevQuoteNo") // EndtPrevQuoteNo
+    private String     endtPrevQuoteNo ;
+    @JsonProperty("EndtCount")  // EndtCount
+    private BigDecimal endtCount ;
+    @JsonProperty("EndtStatus") //EndtStatus
+    private String     endtStatus ;   
+    @JsonProperty("IsFinanceEndt") //IsFinanceEndt
+    private String     isFinaceYn ;  
+    @JsonProperty("EndtCategoryDesc") //EndtCategoryDesc
+    private String     endtCategDesc ;
+    @JsonProperty("EndorsementType") //EndorsementType
+    private Integer    endorsementType ;
+
+    @JsonProperty("EndorsementTypeDesc") // EndorsementTypeDesc
+    private String     endorsementTypeDesc ;
+    
+ 
 
 }

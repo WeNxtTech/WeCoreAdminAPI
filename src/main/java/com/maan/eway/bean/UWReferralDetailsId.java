abce3ae740094a10848d43afdea20e3debc366d0
Suagnya <89253083+MaanRsa001@users.noreply.github.com>
2023-09-01 15:53:05 +0530

    entity

---
+++ src/main/java/com/maan/eway/bean/UWReferralDetailsId.java
@@ -0,0 +1,16 @@
+package com.maan.eway.bean;
+
+import java.io.Serializable;
+
+public class UWReferralDetailsId  implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	
+    private String     requestReferenceNo ;
+    private Integer    productId ;
+    private String    uwLoginId ;
+    private String     companyId ;
+
+	
+}
---
+++ src/main/java/com/maan/eway/bean/UWReferralHistoryId.java
@@ -0,0 +1,16 @@
+package com.maan.eway.bean;
+
+import java.io.Serializable;
+
+public class UWReferralHistoryId  implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	
+    private String     requestReferenceNo ;
+    private Integer    productId ;
+    private String    uwLoginId ;
+    private String     companyId ;
+
+	
+}

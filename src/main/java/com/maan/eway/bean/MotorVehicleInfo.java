2fdb49b6f70cb9340e54b099f0acb6a5907cb65a
Samilla <89253083+MaanRsa001@users.noreply.github.com>
2023-11-08 17:44:17 +0530

    Update MotorVehicleInfo.java

--- src/main/java/com/maan/eway/bean/MotorVehicleInfo.java
+++ src/main/java/com/maan/eway/bean/MotorVehicleInfo.java
@@ -15,7 +15,6 @@ package com.maan.eway.bean;
 
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
-import javax.persistence.Table;
 
 import lombok.*;
 import javax.persistence.*;
@@ -166,6 +165,8 @@ private static final long serialVersionUID = 1L;
     private String     policyYn ;
 
 
+    @Column(name="MODEL_NUMBER", length=30)
+    private String     ModelNumber;
     //--- ENTITY LINKS ( RELATIONSHIP )
 
 

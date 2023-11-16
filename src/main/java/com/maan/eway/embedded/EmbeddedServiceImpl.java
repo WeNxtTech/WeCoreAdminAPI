fc455714a517f1afe36de7dd42ea532999221070
MaanRsa001 <89253083+MaanRsa001@users.noreply.github.com>
2023-11-16 16:52:06 +0530

    inalipa issue

--- src/main/java/com/maan/eway/embedded/EmbeddedServiceImpl.java
+++ src/main/java/com/maan/eway/embedded/EmbeddedServiceImpl.java
@@ -264,7 +264,7 @@ public class EmbeddedServiceImpl implements EmbeddedService {
 						.expiryPolicyCount(premium.get("expiryPolicyCount")==null?"0":premium.get("expiryPolicyCount").toString())
 						.expiryPolicyPremium(premium.get("expiry_premium")==null?"0":df.format(premium.get("expiry_premium")))
 						.activePremium(premium.get("active_premium")==null?"0":df.format(premium.get("active_premium")))
-						.filePath(premium.get("pdf_path")==null?"0":df.format(premium.get("pdf_path")))
+						.filePath(premium.get("pdf_path")==null?"0":(String)premium.get("pdf_path"))
 						.build();
 				return boardRes;
 			}).collect(Collectors.toList());
@@ -317,7 +317,7 @@ public class EmbeddedServiceImpl implements EmbeddedService {
 						.expiryPolicyCount(premium.get("expiryPolicyCount")==null?"0":premium.get("expiryPolicyCount").toString())
 						.expiryPolicyPremium(premium.get("expiry_premium")==null?"0":df.format(premium.get("expiry_premium")))
 						.activePremium(premium.get("active_premium")==null?"0":df.format(premium.get("active_premium")))
-						.filePath(premium.get("pdf_path")==null?"0":df.format(premium.get("pdf_path")))
+						.filePath(premium.get("pdf_path")==null?"0":(String)premium.get("pdf_path"))
 						.build();
 					return boardRes;
 			}).collect(Collectors.toList());
@@ -370,7 +370,7 @@ public class EmbeddedServiceImpl implements EmbeddedService {
 						.expiryPolicyCount(premium.get("expiryPolicyCount")==null?"0":premium.get("expiryPolicyCount").toString())
 						.expiryPolicyPremium(premium.get("expiry_premium")==null?"0":df.format(premium.get("expiry_premium")))
 						.activePremium(premium.get("active_premium")==null?"0":df.format(premium.get("active_premium")))
-						.filePath(premium.get("pdf_path")==null?"0":df.format(premium.get("pdf_path")))
+						.filePath(premium.get("pdf_path")==null?"0":(String)premium.get("pdf_path"))
 						.build();
 					return boardRes;
 			}).collect(Collectors.toList());

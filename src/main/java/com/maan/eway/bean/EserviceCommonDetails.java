f16ced4277c52c2823ce8e0250f5539195ee11ad
BanuLekha <89253083+MaanRsa001@users.noreply.github.com>
2023-12-02 12:58:22 +0530

    changes in public liability

--- src/main/java/com/maan/eway/bean/EserviceCommonDetails.java
+++ src/main/java/com/maan/eway/bean/EserviceCommonDetails.java
@@ -415,6 +415,18 @@ private static final long serialVersionUID = 1L;
     @Column(name="BROKER_TIRA_CODE")
     private String brokerTiraCode;
 
+       @Column(name = "ANY_ACCIDENT_SI")
+       private BigDecimal anyAccidentSi;
+       
+       @Column(name = "ANY_ACCIDENT_SI_LC")
+       private BigDecimal anyAccidentSiLc;
+       
+       @Column(name = "INSURANCE_PERIOD_SI")
+       private BigDecimal insurancePeriodSi;
+       
+       @Column(name = "INSURANCE_PERIOD_SI_LC")
+       private BigDecimal insurancePeriodSiLc;
+       
 }
 
 

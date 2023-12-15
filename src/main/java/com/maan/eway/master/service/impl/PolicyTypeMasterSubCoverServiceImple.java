0629249a169dad442a8ec598c6a4faf7ef338e30
Maanrsa001 <89253083+MaanRsa001@users.noreply.github.com>
2023-12-15 11:35:29 +0530

    plan

--- src/main/java/com/maan/eway/master/service/impl/PolicyTypeMasterSubCoverServiceImple.java
+++ src/main/java/com/maan/eway/master/service/impl/PolicyTypeMasterSubCoverServiceImple.java
@@ -449,8 +449,9 @@ public class PolicyTypeMasterSubCoverServiceImple  implements PolicyTypeMasterSu
 			Predicate a7 = cb.equal(ocpm1.get("branchCode"), b.get("branchCode"));
 			Predicate a9 = cb.equal(ocpm1.get("coverId"),  b.get("coverId"));
 			Predicate a10 = cb.equal(ocpm1.get("subCoverId"),  b.get("subCoverId"));
+			Predicate a11 = cb.notEqual(ocpm1.get("subCoverId"),  "0");
 	
-			amendId.where(a1,a2,a3,a4,a5,a7,a9,a10);
+			amendId.where(a1,a2,a3,a4,a5,a7,a9,a10, a11);
 
 			Predicate n1 = cb.equal(b.get("amendId"),amendId);
 			Predicate n2 = cb.equal(b.get("productId"), req.getProductId() );	
@@ -462,9 +463,10 @@ public class PolicyTypeMasterSubCoverServiceImple  implements PolicyTypeMasterSu
 //			Predicate n8 = cb.lessThanOrEqualTo(b.get("effectiveStartdate"), today);
 //			Predicate n9 = cb.greaterThanOrEqualTo(b.get("effectiveEnddate"), todayEnd);
 			Predicate n10 = cb.equal(b.get("subCoverId"), req.getSubCoverId());
+			Predicate n11 = cb.notEqual(b.get("subCoverId"),  "0");
 			
 		
-			query.where(n1, n2,n3,n4, n5, n6, n7, n10);
+			query.where(n1, n2,n3,n4, n5, n6, n7, n10, n11);
 			
 			TypedQuery<TravelPolicyType> result = em.createQuery(query);
 			list = result.getResultList();
@@ -608,8 +610,9 @@ public class PolicyTypeMasterSubCoverServiceImple  implements PolicyTypeMasterSu
 				Predicate a4 = cb.equal(ocpm1.get("planTypeId"),b.get("planTypeId"));
 				Predicate a5 = cb.equal(ocpm1.get("coverId"),b.get("coverId"));
 				Predicate a7 = cb.equal(ocpm1.get("branchCode"), b.get("branchCode"));
+				Predicate a8 = cb.notEqual(ocpm1.get("subCoverId"),  "0");
 		
-				amendId.where(a1,a2,a3,a4,a5,a7);
+				amendId.where(a1,a2,a3,a4,a5,a7, a8);
 
 				Predicate n1 = cb.equal(b.get("amendId"),amendId);
 				Predicate n2 = cb.equal(b.get("productId"), req.getProductId() );	
@@ -618,10 +621,11 @@ public class PolicyTypeMasterSubCoverServiceImple  implements PolicyTypeMasterSu
 				Predicate n5 = cb.equal(b.get("coverId"), req.getCoverId());
 				Predicate n6 =  cb.equal(b.get("policyTypeId"), req.getPolicyTypeId()); 
 				Predicate n7 =  cb.equal(b.get("planTypeId"), req.getPlanTypeId()); 
+				Predicate n8 = cb.notEqual(b.get("subCoverId"),  "0");
 //				Predicate n8 = cb.lessThanOrEqualTo(b.get("effectiveStartdate"), today1);
 //				Predicate n9 = cb.greaterThanOrEqualTo(b.get("effectiveEnddate"), todayEnd1);
 			
-				query.where(n1, n2,n3,n4, n5, n6, n7);
+				query.where(n1, n2,n3,n4, n5, n6, n7, n8);
 				
 				TypedQuery<TravelPolicyType> result = em.createQuery(query);
 				list = result.getResultList();
@@ -702,8 +706,9 @@ public class PolicyTypeMasterSubCoverServiceImple  implements PolicyTypeMasterSu
 				Predicate n6 =  cb.equal(b.get("policyTypeId"), req.getPolicyTypeId()); 
 				Predicate n7 =  cb.equal(b.get("planTypeId"), req.getPlanTypeId()); 
 				Predicate n10 = cb.equal(b.get("subCoverId"), req.getSubCoverId());
+				Predicate n11 = cb.notEqual(b.get("subCoverId"),  "0");
 		
-				query.where(n2,n3,n4, n5, n6, n7,n10) ;
+				query.where(n2,n3,n4, n5, n6, n7,n10, n11) ;
 			
 				TypedQuery<TravelPolicyType> result = em.createQuery(query);
 				int limit=0, offset=2;

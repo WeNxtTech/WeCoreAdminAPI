420d07cd9cdc4c66261dc7a803a893da49b978d0
suganya <89253083+MaanRsa001@users.noreply.github.com>
2024-01-29 17:51:21 +0530

    car alarm key addes

--- src/main/java/com/maan/eway/bean/EserviceMotorDetails.java
+++ src/main/java/com/maan/eway/bean/EserviceMotorDetails.java
@@ -610,6 +610,9 @@ private static final long serialVersionUID = 1L;
     @Column(name="VEHICLE_GROUP", length=20)
     private String     vehicleGroup ;
     
+    @Column(name="CAR_ALARM_YN", length=20)
+    private String     carAlarmYn ;
+    
 }
 
 

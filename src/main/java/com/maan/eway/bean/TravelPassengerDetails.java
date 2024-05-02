3c969b9ec7353c2c906ae1a9e972402594f0652e
sneha <89253083+MaanRsa001@users.noreply.github.com>
2024-05-02 10:40:11 +0530

    Removed ratingRelationId ,ratingRelationDesc in TravelPassengerDetails

--- src/main/java/com/maan/eway/bean/TravelPassengerDetails.java
+++ src/main/java/com/maan/eway/bean/TravelPassengerDetails.java
@@ -386,11 +386,7 @@ private static final long serialVersionUID = 1L;
     @Column(name="ENDT_VAT_PREMIUM")
     private BigDecimal endtVatPremium;
     
-    @Column(name="RATING_RELATION_ID")
-    private String ratingRelationId;
-
-    @Column(name="RATING_RELATION_DESC")
-    private String ratingRelationDesc;
+  
 
     
 }

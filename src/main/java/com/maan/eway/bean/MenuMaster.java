866488053ec3646b4d9681fa52d9a5e0a4c88bd1
Common 42 System <89253083+MaanRsa001@users.noreply.github.com>
2024-05-02 13:39:33 +0530

    Revert "menumaster changes"
    This reverts commit 9ae3a7de0e3ef576bab7ab1adade6a53aaabbd27.

--- src/main/java/com/maan/eway/bean/MenuMaster.java
+++ src/main/java/com/maan/eway/bean/MenuMaster.java
@@ -14,11 +14,11 @@ package com.maan.eway.bean;
 
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
+import javax.persistence.Table;
 
 import lombok.*;
 import javax.persistence.*;
 import java.io.Serializable;
-import java.util.Date;
 import java.util.List;
 
 import javax.persistence.*;
@@ -41,7 +41,6 @@ import javax.persistence.*;
 @DynamicInsert
 @DynamicUpdate
 @Builder
-@IdClass(MenuMasterId.class)
 @Table(name="eway_menu_master")
 
 
@@ -53,15 +52,6 @@ private static final long serialVersionUID = 1L;
     @Id
     @Column(name="MENU_ID", nullable=false)
     private Integer    menuId ;
-    
-
-    @Id
-    @Column(name="USERTYPE", length=100,nullable=false)
-    private String     usertype ;
-    
-    @Id
-    @Column(name="COMPANY_ID", length=50,nullable=false)
-    private String     companyId ;
 
     //--- ENTITY DATA FIELDS 
     @Column(name="MENU_NAME", length=300)
@@ -85,7 +75,8 @@ private static final long serialVersionUID = 1L;
     @Column(name="RSACODE", length=25)
     private String     rsacode ;
 
-
+    @Column(name="USERTYPE", length=100)
+    private String     usertype ;
 
     @Column(name="ISCLICK", length=100)
     private String     isclick ;
@@ -98,12 +89,6 @@ private static final long serialVersionUID = 1L;
 
     @Column(name="MENU_LOGO", length=200)
     private String     menuLogo ;
-    
-    @Column(name="CREATED_BY")
-   	private String createdBy;
-       
-       @Column(name="ENTRY_DATE")
-   	private Date entryDate;
 
 
     //--- ENTITY LINKS ( RELATIONSHIP )
--- src/main/java/com/maan/eway/repository/MenuMasterRepository.java
+++ src/main/java/com/maan/eway/repository/MenuMasterRepository.java
@@ -7,10 +7,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.maan.eway.bean.MenuMaster;
-import com.maan.eway.bean.MenuMasterId;
 
 
-public interface MenuMasterRepository  extends JpaRepository<MenuMaster, MenuMasterId>, JpaSpecificationExecutor<MenuMaster> {
+public interface MenuMasterRepository  extends JpaRepository<MenuMaster, Integer>, JpaSpecificationExecutor<MenuMaster> {
 
 
                                                                               

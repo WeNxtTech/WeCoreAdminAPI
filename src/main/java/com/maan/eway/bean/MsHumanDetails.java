310f3d17e19f7cda93cb24c712f610cff3458457
sneha <89253083+MaanRsa001@users.noreply.github.com>
2024-10-05 13:34:54 +0530

    Count Key Add

--- src/main/java/com/maan/eway/bean/MsHumanDetails.java
+++ src/main/java/com/maan/eway/bean/MsHumanDetails.java
@@ -283,6 +283,9 @@ private static final long serialVersionUID = 1L;
 	    
 	    @Column(name="DOMESTIC_SERVENT_SI_LC")
 	    private BigDecimal       domesticServentSiLc;
+	    
+	    @Column(name="COUNT")
+	    private Integer count;
 }
 
 
